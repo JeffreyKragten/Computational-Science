584bbeae5cf3d56139decf70afad3baf0a461e98
Luuk van der Waals <luuk.van.der.waals@student.uva.nl>
2023-01-23 13:44:23 +0100

    Changed graph to function for import

--- graph.py
+++ graph.py
@@ -2,25 +2,26 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-if len(sys.argv) > 1:
-    category = sys.argv[1]
-else:
-    category = "percentage_signers"
+def create_graph(args=[]):
+    category = args[1] if len(args) > 1 else "percentage_signers"
+    savefile = "{}/results/{}".format(sys.path[0], "results", args[2]) if len(args) > 2 else None
+    filename = "{}/results/results.csv".format(sys.path[0])
 
-filename = "{}/{}".format(sys.path[0], "results/results.csv")
-with open(filename) as f:
-    categories = f.readline().rstrip().split(",")
-data = np.loadtxt(filename, skiprows=1, delimiter=",")
-print(categories)
+    with open(filename) as f:
+        categories = f.readline().rstrip().split(",")
+    data = np.loadtxt(filename, skiprows=1, delimiter=",")
 
-steps = int(max(data[:,categories.index("Step")])) + 1
-category_data = data[:,categories.index(category)].reshape((-1, steps))
+    steps = int(max(data[:,categories.index("Step")])) + 1
+    category_data = data[:,categories.index(category)].reshape((-1, steps))
 
-plt.plot(np.median(category_data, axis=0))
-plt.fill_between(np.arange(steps),
-                *np.percentile(category_data, [25, 75], axis=0), alpha=.2)
+    plt.plot(np.median(category_data, axis=0))
+    plt.fill_between(np.arange(steps),
+                    *np.percentile(category_data, [25, 75], axis=0), alpha=.2)
 
-if len(sys.argv) > 3:
-    plt.savefig(sys.argv[3])
-else:
-    plt.show()
+    if savefile:
+        plt.savefig(savefile)
+    else:
+        plt.show()
+
+if __name__ == "__main__":
+    create_graph(sys.argv)
